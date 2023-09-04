3a871fe3287de9ec32f58f7cdc7fd93f76b09a9f
Zakhar Voit <zvoit@igalia.com>
2023-09-04 13:16:00 +0800

    npm run dist

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 1.1.0 (Date 2023-09-04, Commit #e8d6c7ee)');
+console.log('A-Frame Version: 1.1.0 (Date 2023-09-04, Commit #6b2318f4)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
